50b1291a9ac5d5cf5094da1d77391091b82a4a1e
s-kawabe <s.kawabe2281@gmail.com>
2021-12-13 23:06:36 +0900

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM postgres:14-alpine
+
+ENV TZ Asia/Tokyo
+
+ENV LANG ja_JP.UTF-8
+ENV LANGUAGE ja_JP:ja
+ENV LC_ALL ja_JP.UTF-8
---
+++ app/server/entrypoints/index.ts
@@ -0,0 +1,6 @@
+import { init } from '$/service/app'
+import { API_SERVER_PORT } from '$/service/envValues'
+
+
+init().listen(API_SERVER_PORT, '0.0.0.0')
+
