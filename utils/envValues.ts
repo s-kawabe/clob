9497699b19c7fac4ca8a3e35d96854d99c473729
s-kawabe <s.kawabe2281@gmail.com>
2021-12-19 13:01:17 +0900

    chore: add auth0 and env values

---
+++ utils/envValues.ts
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const NEXT_PUBLIC_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL_PORT ?? ''
+const NEXT_PUBLIC_AUTH0_DOMAIN = process.env.NEXT_PUBLIC_AUTH0_DOMAIN ?? ''
+const NEXT_PUBLIC_AUTH0_CLIENT_ID =
+  process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID ?? ''
+
+export {
+  NEXT_PUBLIC_AUTH0_CLIENT_ID,
+  NEXT_PUBLIC_AUTH0_DOMAIN,
+  NEXT_PUBLIC_BASE_URL
+}
